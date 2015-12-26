726df687746318020ff33b72a8f64718603ff32f
Dmitry Pospelov <Dim@Dmitrys-MacBook-Pro.local>
2015-12-26 18:36:36 -0500

    Demo

--- index.js
+++ index.js
@@ -9,12 +9,16 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
+app.get('/', function (req, res) {
+  //response.render('pages/index');
+  if (req.query.u != 'key')
+  	return res.end(':(\n') 
+
+  return res.end(':)\n') 
+
 });
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
 
