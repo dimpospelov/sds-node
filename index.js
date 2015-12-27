d1764a4c8db00d71a13a5e3fc4a5719fc6f0e32b
Dmitry Pospelov <Dim@Dmitrys-MacBook-Pro.local>
2015-12-26 21:53:04 -0500

    Demo

--- index.js
+++ index.js
@@ -5,6 +5,17 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
+var MailChimpAPI = require('mailchimp').MailChimpAPI;
+var mailchimp_api = 'a73945ca916173c3fa7b75cb0fe32269-us12';
+var mailchimp_list_id = 'fa1a2a4e7b';
+
+try { 
+    var api = new MailChimpAPI(mailchimp_api, { version : '2.0' });
+} catch (error) {
+    console.log(error.message);
+}
+
+
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
@@ -32,12 +43,24 @@ app.post('/', function (req, res){
     for (var i=0; i<mandrill_events.length; i++) {
     	if (mandrill_events[i]['type'] == 'blacklist' &&
     		mandrill_events[i]['action'] == 'add') {
-    		console.log(mandrill_events[i]['reject']['reason']);
-    		console.log(mandrill_events[i]['reject']['email']);
+    		//console.log(mandrill_events[i]['reject']['reason']);
+    		//console.log(mandrill_events[i]['reject']['email']);
+
+			api.call('lists', 'subscribe', { id: mailchimp_list_id, email: { email: mandrill_events[i]['reject']['email'] }, double_optin: false}, function (error, data) {
+			    if (error)
+			        console.log(error.message);
+			    else
+			        console.log(JSON.stringify(data)); // Do something with your data!
+
+				api.call('lists', 'unsubscribe', { id: mailchimp_list_id, email: { email: mandrill_events[i]['reject']['email'] }}, function (error, data) {
+				    if (error)
+				        console.log(error.message);
+				    else
+				        console.log(JSON.stringify(data)); // Do something with your data!
+				});
+
+			});
     	}
-
-
-
 	}    	
 
     res.writeHead(200, {'Content-Type': 'text/html'});
