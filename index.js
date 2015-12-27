8b97f36c496042fc221d7aef903997cd3fb78ac3
Dmitry Pospelov <Dim@Dmitrys-MacBook-Pro.local>
2015-12-26 19:23:06 -0500

    Demo

--- index.js
+++ index.js
@@ -26,8 +26,10 @@ app.get('/', function (req, res) {
 
 app.post('/', function (req, res){
     console.log('POST /');
-    console.log(req.query.u);
-    //console.log(req.body);
+
+    if (req.query.u !- 'key')
+    	return res.end('Wrong key')
+    	
 
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Post request received');
