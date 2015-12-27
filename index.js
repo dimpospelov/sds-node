1c0bb05a466b6324176a0b3e47ec06fc76f2d6c5
Dmitry Pospelov <Dim@Dmitrys-MacBook-Pro.local>
2015-12-26 20:04:30 -0500

    Demo

--- index.js
+++ index.js
@@ -28,7 +28,9 @@ app.post('/', function (req, res){
     	return res.end('Wrong key');
 
     var mandrill_events = req.body.mandrill_events;
-    console.log(mandrill_events["event"]);
+    console.log(mandrill_events["0"]["event"]);
+    console.log(mandrill_events.event);
+    console.log(mandrill_events.0.event);
     	
 
     res.writeHead(200, {'Content-Type': 'text/html'});
